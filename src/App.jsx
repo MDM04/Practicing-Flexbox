0f094bde36338f10a67a7bf0561b1c68d4f1ce65
MDM <mdm.pro4@gmail.com>
2024-01-31 13:50:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+const App =()=>{
+  return(
+    <>
+        <div className='container-item1'>
+     <div className='item-1'>
+        <p>Item 1</p>
+      </div>
+     </div>
+    <div className="container">
+     
+      <div className='item'>
+        <p>Item 2</p>
+      </div>
+      <div className='item'>
+        <p>Item 3</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+
+export default App
